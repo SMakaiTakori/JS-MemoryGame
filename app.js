dda3ece1b762094577a2f513a55f638f54c389c0
SMakaiTakori <makait87@icloud.com>
2021-03-14 21:02:30 -0400

    created cards arrays

---
+++ app.js
@@ -0,0 +1,53 @@
+document.addEventListener("DOMContentLoaded", () => {
+  //create an array of cards to use later
+  const cardArray = [
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "fries",
+      img: "images/fries.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
+      name: "ice cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "ice cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+  ];
+});
