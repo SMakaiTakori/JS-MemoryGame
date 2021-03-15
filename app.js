fb701d0db3264f2d5384ab8b4b8a2db747e3f9dc
SMakaiTakori <makait87@icloud.com>
2021-03-15 16:51:19 -0400

    updated checkForMatch function to include condition for clicking the same image

--- app.js
+++ app.js
@@ -6,6 +6,26 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/fries.png",
     },
     {
+      name: "cheeseburger",
+      img: "images/cheeseburger.png",
+    },
+    {
+      name: "ice-cream",
+      img: "images/ice-cream.png",
+    },
+    {
+      name: "pizza",
+      img: "images/pizza.png",
+    },
+    {
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
+    },
+    {
       name: "fries",
       img: "images/fries.png",
     },
@@ -14,40 +34,20 @@ document.addEventListener("DOMContentLoaded", () => {
       img: "images/cheeseburger.png",
     },
     {
-      name: "cheeseburger",
-      img: "images/cheeseburger.png",
-    },
-    {
-      name: "hotdog",
-      img: "images/hotdog.png",
-    },
-    {
-      name: "hotdog",
-      img: "images/hotdog.png",
-    },
-    {
-      name: "ice cream",
+      name: "ice-cream",
       img: "images/ice-cream.png",
-    },
-    {
-      name: "ice cream",
-      img: "images/ice-cream.png",
-    },
-    {
-      name: "milkshake",
-      img: "images/milkshake.png",
-    },
-    {
-      name: "milkshake",
-      img: "images/milkshake.png",
     },
     {
       name: "pizza",
       img: "images/pizza.png",
     },
     {
-      name: "pizza",
-      img: "images/pizza.png",
+      name: "milkshake",
+      img: "images/milkshake.png",
+    },
+    {
+      name: "hotdog",
+      img: "images/hotdog.png",
     },
   ];
 
@@ -64,27 +64,34 @@ document.addEventListener("DOMContentLoaded", () => {
     //loop through the cardsArray
     for (let i = 0; i < cardArray.length; i++) {
       //for each card create an image element
-      var card = document.createElement("img");
+      const card = document.createElement("img");
       //initially the card will be blank
       card.setAttribute("src", "images/blank.png");
       card.setAttribute("data-id", i);
       //flipcard function to be called on click of each card
-      card.addEventListener("click", flipcard);
+      card.addEventListener("click", flipCard);
       //append each card to the grid element
       grid.appendChild(card);
     }
   };
 
   //Check for matches
-  const checkForMatch = () => {
-    var cards = document.querySelectorAll("img");
+  function checkForMatch() {
+    const cards = document.querySelectorAll("img");
     //we want the first value in this array and assign to a variable
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
-    if (cardsChosen[0] === cardsChosen[1]) {
+
+    if (optionOneId === optionTwoId) {
+      cards[optionOneId].setAttribute("src", "images/blank.png");
+      cards[optionTwoId].setAttribute("src", "images/blank.png");
+      alert("You have clicked the same image!");
+    } else if (cardsChosen[0] === cardsChosen[1]) {
       alert("You found a match");
       cards[optionOneId].setAttribute("src", "images/white.png");
       cards[optionTwoId].setAttribute("src", "images/white.png");
+      cards[optionOneId].removeEventListener("click", flipCard);
+      cards[optionTwoId].removeEventListener("click", flipCard);
       cardsWon.push(cardsChosen);
     } else {
       cards[optionOneId].setAttribute("src", "images/blank.png");
@@ -97,22 +104,22 @@ document.addEventListener("DOMContentLoaded", () => {
     if (cardsWon.length === cardArray.length / 2) {
       resultDisplay.textContent = "Congratulations! You found them all!";
     }
-  };
+  }
 
   //Flip your card
 
-  const flipcard = () => {
-    var cardId = this.getAttribute("data-id");
+  function flipCard() {
+    let cardId = this.getAttribute("data-id");
     //create an array of cards chosen
     cardsChosen.push(cardArray[cardId].name);
-    cardsChosenId.push(cardArray[cardId]);
+    cardsChosenId.push(cardId);
     //add an image to the square that is clicked to invoke this function
     //based on the cardId it holds
     this.setAttribute("src", cardArray[cardId].img);
     if (cardsChosen.length === 2) {
       setTimeout(checkForMatch, 500);
     }
-  };
+  }
 
   createBoard();
 });
